9e5dd69d6a92e7f29ea2d5a56fe7f9c272eb8cc9
gyeong3un2 <gyeong3un2@gmail.com>
2024-05-13 07:14:49 +0900

    feat: ch2-faker.js와 /photo/[photoId] 완료

---
+++ ch2-1/src/app/(afterLogin)/_component/Home/Post.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import style from './post.module.css';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import ActionButtons from './ActionButtons';
+import PostArticle from './PostArticle';
+import { faker } from '@faker-js/faker';
+
+dayjs.locale('ko'); // 한글 플러그인 설정
+dayjs.extend(relativeTime); // relativeTime 플러그인 설정
+
+type Props = {
+  noImage?: boolean;
+};
+
+export default function Post({ noImage }: Props) {
+  const target = {
+    postId: 1,
+    User: {
+      id: 'elonmusk',
+      nickname: 'Elon Musk',
+      image: '/yRsRRjGO.jpg',
+    },
+    content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
+    createdAt: new Date(),
+    Images: [] as any[],
+  };
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push({ imageId: 2, link: faker.image.urlLoremFlickr() });
+  }
+
+  return (
+    <PostArticle post={target}>
+      <div className={style.postWrapper}>
+        <div className={style.postUserSection}>
+          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+            <img src={target.User.image} alt={target.User.nickname} />
+            <div className={style.postShade} />
+          </Link>
+        </div>
+        <div className={style.postBody}>
+          <div className={style.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={style.postUserName}>{target.User.nickname}</span>
+              &nbsp;
+              <span className={style.postUserId}>@{target.User.id}</span>
+              &nbsp; · &nbsp;
+            </Link>
+            <span className={style.postDate}>
+              {dayjs(target.createdAt).fromNow(true)}
+            </span>
+          </div>
+          <div>{target.content}</div>
+          <div className={style.postImageSection}>
+            {target.Images && target.Images.length > 0 && (
+              <Link
+                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
+                className={style.postImage}
+              >
+                <img src={target.Images[0].link} alt="" />
+              </Link>
+            )}
+          </div>
+          <ActionButtons />
+        </div>
+      </div>
+    </PostArticle>
+  );
+}
--- ch2-1/src/app/(afterLogin)/search/page.tsx
+++ ch2-1/src/app/(afterLogin)/search/page.tsx
@@ -1,8 +1,43 @@
+import BackButton from '../_component/BackButton';
+import Post from '../_component/Home/Post';
+import SearchForm from '../_component/SearchForm';
+import Tab from './_component/Tab';
+import style from './search.module.css';
 
-export default function Home() {
+type Props = {
+  searchParams: {
+    q: string;
+  };
+};
+
+export default function Search({ searchParams }: Props) {
   return (
-    <main>
-      검색페이지
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm searchParams={{ q: searchParams.q }} />
+          </div>
+        </div>
+        <Tab />
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
     </main>
-  )
+  );
 }

642d984d1cd136bce870cea6b59dacb19dd4067b
경은 <gyeong3un2@gmail.com>
2024-05-14 15:24:56 +0900

    chore: 이전 챕터 소스 제거

--- ch0/src/app/(beforeLogin)/@modal/default.tsx
+++
@@ -1,6 +0,0 @@
-export default function Default() {
-  return null;
-}
-
-// 주소가 localhost:3000일 때는 children->page.tsx, modal->@modal/default.tsx
-// 주소가 localhost:3000/i/flow/login 때는 chldren->i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx

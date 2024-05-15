ed3d20f27cd806e1307046fc5b3b4c07fb83d221
gyeong3un2 <gyeong3un2@gmail.com>
2024-05-16 00:33:48 +0900

    feat: ch4-백엔드 서버 세팅하기 중

--- 0gyeong3un2/src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ 0gyeong3un2/src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,37 +1,51 @@
 'use client';
 
-import {
-  InfiniteData,
-  useInfiniteQuery,
-  useQuery,
-} from '@tanstack/react-query';
+import { InfiniteData, useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
 import Post from '../../_component/Home/Post';
 import { Post as IPost } from '@/model/Post';
 import getPostRecommends from '../_lib/getPostRecommends';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 export default function PostRecommends() {
-  const { data } = useInfiniteQuery<
-    IPost[],
-    Object,
-    InfiniteData<IPost[]>,
-    [_1: string, _2: string],
-    number
-  >({
-    queryKey: ['posts', 'recommends'],
-    queryFn: getPostRecommends,
-    initialPageParam: 0,
-    getNextPageParam: (lastPage) => lastPage.at(-1)?.postId,
-    staleTime: 60 * 1000, // fresh -> stale, 1분이라는 기준
-    gcTime: 60 * 1000 * 5, // stale -> dead, 5분이라는 기준
+  const { data, fetchNextPage, hasNextPage, isFetching } =
+    useSuspenseInfiniteQuery<
+      IPost[],
+      Object,
+      InfiniteData<IPost[]>,
+      [_1: string, _2: string],
+      number
+    >({
+      queryKey: ['posts', 'recommends'],
+      queryFn: getPostRecommends,
+      initialPageParam: 0,
+      getNextPageParam: lastPage => lastPage.at(-1)?.postId,
+      staleTime: 60 * 1000, // fresh -> stale, 1분이라는 기준
+      gcTime: 60 * 1000 * 5, // stale -> dead, 5분이라는 기준
+    });
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+    delay: 0,
   });
 
-  return data?.pages.map((page, i) => (
-    <Fragment key={i}>
-      {page.map((post) => (
-        <Post key={post.postId} post={post} />
+  useEffect(() => {
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [fetchNextPage, hasNextPage, inView, isFetching]);
+
+  return (
+    <>
+      {data?.pages.map((page, i) => (
+        <Fragment key={i}>
+          {page.map(post => (
+            <Post key={post.postId} post={post} />
+          ))}
+        </Fragment>
       ))}
-    </Fragment>
-  ));
+      <div ref={ref} style={{ height: 50 }} />
+    </>
+  );
 }
--- 0gyeong3un2/src/app/(afterLogin)/home/page.tsx
+++ 0gyeong3un2/src/app/(afterLogin)/home/page.tsx
@@ -2,34 +2,20 @@ import style from './home.module.css';
 import Tab from '@/app/(afterLogin)/home/_component/Tab';
 import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
 import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
-import {
-  HydrationBoundary,
-  QueryClient,
-  dehydrate,
-} from '@tanstack/react-query';
-import getPostRecommends from './_lib/getPostRecommends';
-import PostRecommends from './_component/PostRecommends';
-import TabDecider from './_component/TabDecider';
+import Loading from './loading';
+import { Suspense } from 'react';
+import TabDeciderSuspense from './_component/TabDeciderSuspense';
 
 export default async function Home() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: ['posts', 'recommends'],
-    queryFn: getPostRecommends,
-    initialPageParam: 0,
-  });
-  const hydratedState = dehydrate(queryClient);
-
   return (
     <main className={style.main}>
-      <HydrationBoundary state={hydratedState}>
-        <TabProvider>
-          <Tab />
-          <PostForm />
-          <TabDecider />
-          <PostRecommends />
-        </TabProvider>
-      </HydrationBoundary>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Suspense fallback={<Loading />}>
+          <TabDeciderSuspense />
+        </Suspense>
+      </TabProvider>
     </main>
   );
 }
